234f0433e73d6bb868b16ba48b385e0cb28c5722
liangronghai <18998401056@163.com>
2018-11-27 11:45:36 +0800

    init

---
+++ hero-rpc/hero-rpc-dubbo/src/main/java/org/heroframework/App.java
@@ -0,0 +1,13 @@
+package org.heroframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
